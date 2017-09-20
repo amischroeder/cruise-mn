d9ea8376f18803130919edae3db8a497d1d5d638
Ami Schroeder <arschroeder22@gmail.com>
2017-09-20 11:08:23 -0500

    add delete confirmation dialog box

--- server/public/scripts/controllers/material.controller.js
+++ server/public/scripts/controllers/material.controller.js
@@ -1,7 +1,28 @@
-myApp.controller('MaterialController', function($mdDialog, $mdToast) {
+myApp.controller('MaterialController', ['PathService', '$mdDialog', '$mdToast','$routeParams', '$location', function(PathService, $mdDialog, $mdToast, $routeParams, $location) {
     console.log('MaterialController Loaded');
     
     var vm = this;
+
+    vm.deletePath = function () {
+        console.log('pathID:', $routeParams.id);
+        PathService.deletePath($routeParams.id);
+        $location.path("/skatepaths");
+    };
+
+    vm.status = '  ';
+    vm.showConfirm = function(ev) {
+        var confirm = $mdDialog.confirm()
+        .title('Do you really want to delete this path?')
+        .targetEvent(ev)
+        .ok('Yep!')
+        .cancel('Nevermind');
+        $mdDialog.show(confirm).then(function() {
+            // vm.status = 'Deleted!';
+            vm.deletePath();
+          }, function() {
+            vm.status = 'Okay lets leave it there';
+          });
+    };
 
 
     vm.showToast = function (ev) {
@@ -10,4 +31,4 @@ myApp.controller('MaterialController', function($mdDialog, $mdToast) {
             .textContent('Path was posted! Check it out on the listing page!')
         );
     }
-});
+}]);
