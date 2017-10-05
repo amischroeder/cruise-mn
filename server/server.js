07ef33174c9ac6314c1a20ea963a0577c1d47e2a
Ami Schroeder <arschroeder22@gmail.com>
2017-10-05 16:28:22 -0500

    last changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pg = require('pg');
 
 var passport = require('./strategies/sql.localstrategy');
 var sessionConfig = require('./modules/session.config');
@@ -12,6 +13,7 @@ var registerRouter = require('./routes/register.router');
 var skatepaths = require('./routes/path.router');
 
 var port = process.env.PORT || 5000;
+
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -40,3 +42,15 @@ app.use('/', indexRouter);
 app.listen(port, function(){
    console.log('Listening on port:', port);
 });
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: result.rows} ); }
+      });
+    });
+  });
